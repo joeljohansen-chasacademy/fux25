2b2706e84d68e47d41dc13e210f57da09c209a88
joeljanson <joel.janson.johansen@gmail.com>
2025-09-30 14:39:20 +0200

    added one more example

--- js/30/stopwatch.js
+++ js/30/stopwatch.js
@@ -1,5 +1,6 @@
 let swStartTime = 0;
 let swElapsed = 0;
+let swElapsedTwo = 0;
 let swTimerId = null;
 const tickSpeedMs = 10;
 
@@ -7,9 +8,17 @@ const swMinutes = document.getElementById("swMinutes");
 const swSeconds = document.getElementById("swSeconds");
 const swMilliseconds = document.getElementById("swMilliseconds");
 
+const swMinutesTwo = document.getElementById("swMinutes-2");
+const swSecondsTwo = document.getElementById("swSeconds-2");
+const swMillisecondsTwo = document.getElementById("swMilliseconds-2");
+
 const btnStart = document.getElementById("swStart");
 const btnStop = document.getElementById("swStop");
 const btnReset = document.getElementById("swReset");
+
+let millieSeconds = 0;
+let seconds = 0;
+let minutes = 0;
 
 btnStart.addEventListener("click", () => {
 	if (swTimerId) return; // En timer är redan igång! Starta inte en ny.
@@ -20,14 +29,6 @@ btnStart.addEventListener("click", () => {
 		const sec = Math.floor(swElapsed / 1000) % 60;
 		const min = Math.floor(swElapsed / 60000);
 
-		//console.log("Millisekunder med modulo", ms);
-		//console.log("Millisekunder utan modulo", swElapsed);
-		/* console.log(
-			"Sekunder som passerat utan modulo",
-			Math.floor(swElapsed / 1000)
-		);
-		console.log("Sekunder som passerat med modulo", sec); */
-
 		swMilliseconds.textContent = ms;
 		swSeconds.textContent = sec;
 		swMinutes.textContent = min;
@@ -35,6 +36,24 @@ btnStart.addEventListener("click", () => {
 	btnStart.disabled = true;
 	btnStop.disabled = false;
 	btnReset.disabled = false;
+	/* 
+    //Ett annat sätt att lösa uppgiften på där vi inte håller reda på tiden utan låter performance.now()
+    //visa oss.
+	let timeBeforeStart = performance.now();
+
+	swTimerId = setInterval(() => {
+		swElapsedTwo = performance.now() - timeBeforeStart;
+
+		console.log(swElapsedTwo);
+
+		const ms = swElapsedTwo % 1000;
+		const sec = Math.floor(swElapsedTwo / 1000) % 60;
+		const min = Math.floor(swElapsedTwo / 60000);
+
+		swMillisecondsTwo.textContent = ms;
+		swSecondsTwo.textContent = sec;
+		swMinutesTwo.textContent = min;
+	}, tickSpeedMs); */
 });
 
 btnStop.addEventListener("click", () => {
